6968a1d348d22606c729a8553af6cc97f738470d
Julio Daniel Azurduy <113427338+DanJuaz@users.noreply.github.com>
2023-10-20 14:25:36 +0000

    star and score

--- script.js
+++ script.js
@@ -5,7 +5,7 @@
     var contex2 = canvas2.getContext('2d');
     
     // Score
-    var score = 20;
+    var score = 0;
     //Boundary
     var limitTop = 0;
     var limitBottom = canvas.height ;
@@ -17,29 +17,37 @@
     var radius = 5;
     // V and G
     var angle = Math.PI/2;
-    var velY = -3.5;
-    var velX = 3.5;
+    var velY = -4;
+    var velX = 4;
     // Rectangle
     var rectangleWidth = canvas2.width/3;
     var rectangleHeight = canvas2.height/4;
     var rectangleX = (canvas.width - rectangleWidth) / 2;
     var rectangleY = canvas2.height * 20;
 
+    var starPositions = [
+        { x: canvas.width / 20, y: canvas.height / 10 },
+        { x: canvas.width / 3, y: canvas.height / 7 }, 
+        { x: canvas.width * 19 / 20, y: canvas.height / 2 } 
+    ];
+
     //Start
     function start(){
         draw();
         //drawStar();
-        update();
+        moveBall();
         document.addEventListener('keydown',keydown);
     }
     //Draw
     function draw(){
-        
         context.clearRect(0, 0, canvas.width, canvas.height);   
         drawBall();
         drawRectangle();
         drawScore();
         drawStar();
+        for (var i = 0; i < starPositions.length; i++) {
+            drawStar(starPositions[i].x, starPositions[i].y);
+        }
     }
     // Draw ball
     function drawBall(){
@@ -57,39 +65,32 @@
         context.closePath();
         context.fill();
     }
+    //Draw the score
     function drawScore(){
         context.font= "16px Arial";
         context.fillText("Score: "+ score, 8, 20);
     }
     // Draw the Star
-    function drawStar(){
-        var x = canvas.width / random();
-        var y = canvas.height/5; 
-
-        var radius = 25; 
-        var innerRadius = 10; 
-        context.strokeStyle = 'black'; 
-        context.fillStyle = 'yellow'; 
+    function drawStar(x, y) {
+        var radius = 25;
+        var innerRadius = 10;
+        context.strokeStyle = 'black';
+        context.fillStyle = 'yellow';
         context.beginPath();
-
+    
         // Move to the starting point
         context.moveTo(x, y - radius);
-
+    
         // Loop to draw the five points of the star
         for (var i = 0; i < 5; i++) {
-            // Calculate the angle for the outer point
             var angle = ((Math.PI * 2) / 5) * i - Math.PI / 2;
-            
-            // Calculate the coordinates for the outer point
+    
             var outerX = x + Math.cos(angle) * radius;
             var outerY = y + Math.sin(angle) * radius;
-            
-            // Draw a line to the outer point
+    
             context.lineTo(outerX, outerY);
-            
-            // Calculate the angle for the inner point
             angle += ((Math.PI * 2) / 5) / 2;
-
+    
             var innerX = x + Math.cos(angle) * innerRadius;
             var innerY = y + Math.sin(angle) * innerRadius;
             context.lineTo(innerX, innerY);
@@ -97,13 +98,12 @@
         context.closePath();
         context.fill();
         context.stroke();
-
     }
     //Get math random
     function random() {
         return Math.random() * (20 -1.1) + 1.1;
     }
-    function update(){
+    function moveBall(){
         // Ball boundaries with canvas boundaries
         if(centerX + velX > limitRight || centerX + velX < radius){
             velX = - velX;
@@ -113,9 +113,7 @@
         }
         //Ball bottm bundary and LOSE
         if(centerY + velY > limitBottom){
-            window.alert("Hello world!");
-            alert("Hello world!");
-
+            window.alert(":C, Te rendirás!!");
         }
         // Rectangle with ball boundaries
         if(centerY + radius > rectangleY &&
@@ -125,7 +123,19 @@
              ){
             velY = -velY;
         }
-        
+        // Check for collisions with stars
+        for (var i = 0; i < starPositions.length; i++) {
+        var starX = starPositions[i].x;
+        var starY = starPositions[i].y;
+        var distance = Math.sqrt((centerX - starX) ** 2 + (centerY - starY) ** 2);
+        if (distance < radius + 25) {
+            // Collision detected with a star, increase the score
+            score++;
+            // Remove the star that was hit
+            starPositions.splice(i, 1);
+            // You can add more stars if you want here
+            }
+        }
         // Rectangle boundaries
         if (rectangleX < limitLeft){
             rectangleX = limitLeft;
@@ -135,7 +145,7 @@
         centerX += velX;
         centerY += velY;
         draw();
-        requestAnimationFrame(update);
+        requestAnimationFrame(moveBall);
     }
     // Movement with A/S && Left/Right
     function keydown(event){
