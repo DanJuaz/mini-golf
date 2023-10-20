36ef7cfa0d7b68370d657e95eda19ee45fdbcf28
Julio Daniel Azurduy <113427338+DanJuaz@users.noreply.github.com>
2023-10-20 09:14:14 +0000

    draw first star

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
     var canvas2 = document.getElementById('canvas2');
     var contex2 = canvas2.getContext('2d');
     
-    //
+    // Score
     var score = 20;
     //Boundary
     var limitTop = 0;
@@ -17,11 +17,8 @@
     var radius = 5;
     // V and G
     var angle = Math.PI/2;
-    //var v = 5;
-    //var velX = v * Math.cos(angle);
-    //var velY = v * Math.sin(angle);
-    var velY = -2;
-    var velX = 2;
+    var velY = -3.5;
+    var velX = 3.5;
     // Rectangle
     var rectangleWidth = canvas2.width/3;
     var rectangleHeight = canvas2.height/4;
@@ -40,6 +37,7 @@
         drawBall();
         drawRectangle();
         drawScore();
+        drawMoney();
     }
     // Draw ball
     function drawBall(){
@@ -59,10 +57,68 @@
     }
     function drawScore(){
         context.font= "16px Arial";
-        //context.fillStyle("#FFFFFF");
         context.fillText("Score: "+ score, 8, 20);
     }
+    // Draw the img Money
+    function drawMoney(){
+        // var widthImg = 100;
+        // var heightImg = 100;
+    
+        // var img = new Image();
+        // img.src = "./assets/img/moneda.png";
+    
+        // img.onload = function() {
+        //     context.drawImage(img, 80, 20, widthImg, heightImg);
+        // };
+        // Define the coordinates for the star points
+        var x = canvas.width / 2; // X-coordinate of the center
+        var y = canvas.height / 2; // Y-coordinate of the center
+        var radius = 50; // Radius of the outer circle
+        var innerRadius = 20; // Radius of the inner circle
 
+        // Set the stroke and fill styles
+        context.strokeStyle = 'blue'; // Outline color
+        context.fillStyle = 'yellow'; // Fill color
+
+        // Begin drawing the star
+        context.beginPath();
+
+        // Move to the starting point
+        context.moveTo(x, y - radius);
+
+        // Loop to draw the five points of the star
+        for (var i = 0; i < 5; i++) {
+            // Calculate the angle for the outer point
+            var angle = ((Math.PI * 2) / 5) * i - Math.PI / 2;
+            
+            // Calculate the coordinates for the outer point
+            var outerX = x + Math.cos(angle) * radius;
+            var outerY = y + Math.sin(angle) * radius;
+            
+            // Draw a line to the outer point
+            context.lineTo(outerX, outerY);
+            
+            // Calculate the angle for the inner point
+            angle += ((Math.PI * 2) / 5) / 2;
+            
+            // Calculate the coordinates for the inner point
+            var innerX = x + Math.cos(angle) * innerRadius;
+            var innerY = y + Math.sin(angle) * innerRadius;
+            
+            // Draw a line to the inner point
+            context.lineTo(innerX, innerY);
+        }
+
+        // Close the path to complete the star shape
+        context.closePath();
+
+        // Fill the star with the fill color
+        context.fill();
+
+        // Draw the outline of the star
+        context.stroke();
+
+    }
     function update(){
         // Ball boundaries with canvas boundaries
         if(centerX + velX > limitRight || centerX + velX < radius){
